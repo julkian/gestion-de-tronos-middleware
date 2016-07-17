f70df6b7a845858d1d02fabbe393c264ee5e4558
Julkian <meestoy42@gmail.com>
2016-07-17 20:11:26 +0200

    market leveling up

---
+++ www/src/modules/directives/market/market.component.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function marketDirective(/* inject dependencies here, i.e. : $rootScope */) {
+  return {
+    restrict: 'E',
+    bindToController: {},
+    controller: MarketController,
+    controllerAs: 'market',
+    templateUrl: 'directives/market/market.html'
+  };
+}
+
+MarketController.$inject = ['$rootScope','$gameConstants', 'saveGame'];
+function MarketController($rootScope, $gameConstants, saveGame) {
+  var vm = this;
+  vm.lvlUpMarket = _lvlUpMarket;
+
+  initialize();
+
+  $rootScope.game.totalGold = 100000;
+  $rootScope.game.soldiers = 100000;
+
+  /////////////////////
+
+  function initialize() {
+    vm.marketCost = $gameConstants.MARKET[$rootScope.game.buildings.market+1 + ''].COST;
+    vm.marketDiscount = $gameConstants.MARKET[$rootScope.game.buildings.market + ''].SAVE_PERCENT;
+  }
+
+  function _lvlUpMarket() {
+    $rootScope.game.buildings.market++;
+    vm.marketDiscount = $gameConstants.MARKET[$rootScope.game.buildings.market + ''].SAVE_PERCENT;
+    saveGame.save({gameId: $rootScope.user.gameId}, $rootScope.game).$promise.then(function() {});
+  }
+}
+
+module.exports = marketDirective;
