0f61a931565f8c08e77e8a199589a85478105ebf
Killian Perea Ruiz <killianperea@gmail.com>
2016-07-29 00:48:25 +0200

    include openshift variables WIP

--- constants/application/index.js
+++ constants/application/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    SERVER_PORT: 8080,
-    SERVER_IP: 'localhost',
+    SERVER_PORT: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    SERVER_IP: process.env.OPENSHIFT_NODEJS_IP ||'localhost',
     SECRET: 'someSecret',
     CODE: {
         200: {status: 200, message: 'OK'},
@@ -8,5 +8,5 @@ module.exports = {
         500: {status: 500, message: 'server internal error'}
     },
     TOKEN_EXPIRES: 3153600000000, // 10 years
-    MONGO_IP: 'mongodb://localhost/gestiondetronos'
+    MONGO_IP: 'mongodb://localhost:27017/'
 };
--- server.js
+++ server.js
@@ -9,18 +9,25 @@ var app = express();
 configServer.config(app);
 
 /* MONGODB */
-mongoose.connect(applicationConstants.MONGO_IP);
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'gestiondetronos');
+} else {
+    mongoose.connect(applicationConstants.MONGO_IP + 'gestiondetronos');
+}
 
 /* SERVER ROUTES */
 app.use('/rest', routes.restRoute);
 
 app.use('/site', express.static(__dirname + '/www/release/'));
-app.get('/site/*', function(req, res) {
+app.get('/site/*', function (req, res) {
     res.sendFile(__dirname + '/www/release/index.html');
 });
 
 // Error 404 resource not found
-app.all('*', function(req, res) { res.status(404).send("Recurso no encontrado"); });
+app.all('*', function (req, res) {
+    res.status(404).send("Recurso no encontrado");
+});
 
 /* SERVE */
 var port = applicationConstants.SERVER_PORT;
